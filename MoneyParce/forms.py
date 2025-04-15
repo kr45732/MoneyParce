1479f0239ada36e86fb5d7db9d074710e4f78179
rafael <rgeraldo3@gatech.edu>
2025-04-15 11:24:57 -0400

    Create template that allows users to see and create transactions

---
+++ MoneyParce/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import Expense, Income
+
+class ExpenseForm(forms.ModelForm):
+    class Meta:
+        model = Expense
+        fields = ['amount', 'description', 'category']
+
+class IncomeForm(forms.ModelForm):
+    class Meta:
+        model = Income
+        fields = ['amount', 'description']
--- MoneyParce/views.py
+++ MoneyParce/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from .models import Expense
+
+from .forms import ExpenseForm, IncomeForm
+from .models import Expense, Income
 from .utils import check_budget_status
 
 def dashboard(request):
@@ -8,4 +10,49 @@ def dashboard(request):
     return render(request, 'dashboard.html', {
         'expenses': expenses,
         'warning': warning,
+    })
+
+def transactions(request):
+    # .filter(user=request.user) preferred over .all()
+    expenses = Expense.objects.all()
+    incomes = Income.objects.all()
+    expense_form = ExpenseForm()
+    income_form = IncomeForm()
+
+    return render(request, 'transactions.html', {
+        'expense_form': expense_form,
+        'income_form': income_form,
+        'expenses': expenses,
+        'incomes': incomes,
+    })
+
+def add_transaction(request):
+    expense_form = ExpenseForm()
+    income_form = IncomeForm()
+
+    if request.method == 'POST':
+        if 'add_expense' in request.POST:
+            expense_form = ExpenseForm(request.POST)
+            if expense_form.is_valid():
+                expense = expense_form.save(commit=False)
+                expense.user = request.user
+                expense.save()
+        elif 'add_income' in request.POST:
+            income_form = IncomeForm(request.POST)
+            if income_form.is_valid():
+                income = income_form.save(commit=False)
+                income.user = request.user
+                income.save()
+
+    # Fetch updated transactions
+    # .filter(user=request.user) preferred over .all()
+
+    expenses = Expense.objects.all()
+    incomes = Income.objects.all()
+
+    return render(request, 'transactions.html', {
+        'expense_form': expense_form,
+        'income_form': income_form,
+        'expenses': expenses,
+        'incomes': incomes,
     })
