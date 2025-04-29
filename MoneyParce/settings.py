444ce859cc070a482686efa8376c2c5444030d49
rafael <rgeraldo3@gatech.edu>
2025-04-29 00:50:45 -0400

    Add static files

--- MoneyParce/settings.py
+++ MoneyParce/settings.py
@@ -126,8 +126,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'MoneyParce/static/')
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
